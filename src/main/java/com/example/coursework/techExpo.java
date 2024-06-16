edc463a043a5596081afbfc091cccef0c5945178
Chethaka <071c.krishanthi@gmail.com>
2024-06-16 23:03:49 +0530

    created the all files which we need scene 1

--- src/main/java/com/example/coursework/techExpo.java
+++ src/main/java/com/example/coursework/techExpo.java
@@ -1,4 +1,25 @@
 package com.example.coursework;
 
-public class techExpo {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class techExpo extends Application {
+
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxmls/welcome_user.fxml"));
+        Scene scene1 = new Scene(fxmlLoader.load());
+        scene1.getStylesheets().add(getClass().getResource("stylesheets/scene_1.css").toExternalForm());
+        primaryStage.setTitle("Tech_expo 2.0");
+        primaryStage.setScene(scene1);
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
